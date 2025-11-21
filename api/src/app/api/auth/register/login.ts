29ff1bb93053309f7e6dc5382fde051c7bbc09cb
Fathoni M <diobok@gmail.com>
2025-11-22 06:22:32 +0700

    add: api login

---
+++ api/src/app/api/auth/register/login.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export async function POST(req: Request) {
+  const { email, password } = await req.json();
+
+  const user = await prisma.user.findUnique({
+    where: { email }
+  });
+
+  if (!user) {
+    return Response.json({ error: "User not found" }, { status: 404 });
+  }
+
+  const valid = await bcrypt.compare(password, user.password);
+
+  if (!valid) {
+    return Response.json({ error: "Invalid password" }, { status: 401 });
+  }
+
+  return Response.json({ message: "Login success", user });
+}
