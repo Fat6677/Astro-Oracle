f9f2c3b8fe512fc6a559ba8524049b3943188409
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2025-12-13 01:30:54 +0700

    membuat fitur pemilihan zodiac

---
+++ web/src/app/components/ZodiacSelector.tsx
@@ -0,0 +1,9 @@
+interface ZodiacSelectorProps {
+  onSignSelect: (sign: string) => void;
+  isLoading: boolean;
+}
+
+const zodiacSigns = [
+  'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo',
+  'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'
+];
