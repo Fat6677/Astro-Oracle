ce146c5d728747ed2926c89d43ae9a37c02b2e3e
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2025-12-31 03:15:48 +0700

    header main into layout/mobile

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons'; 
+import Header from '../../components/ui/Header';
+import { View } from 'react-native';
 export default function TabLayout() {
   return (
+    <View style={styles.container}>
+      <Header />
+
+      <Tabs
+        screenOptions={{
+          headerShown: false, 
+          tabBarStyle: {
+            backgroundColor: '#0F172A',
+            borderTopColor: '#1E293B',
+            height: 60,
+            paddingBottom: 10,
+          },
+          tabBarActiveTintColor: '#8B5CF6',
+          tabBarInactiveTintColor: '#94A3B8',
+        }}
+      ></Tabs>
+
     <Tabs
       screenOptions={{
         headerShown: false, 
@@ -17,7 +36,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index" // Merujuk ke index.tsx (Home)
+        name="index" 
         options={{
           title: 'Home',
           tabBarIcon: ({ color, size }) => (
@@ -27,5 +46,6 @@ export default function TabLayout() {
       />
       
     </Tabs>
+    </View>
   );
 }
