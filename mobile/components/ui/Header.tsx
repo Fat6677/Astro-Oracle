3bc52459e7e46255a36e10ffd4a6eab600bfe2c2
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2025-12-31 02:56:21 +0700

    import komponen/mobile

---
+++ mobile/components/ui/Header.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { BlurView } from 'expo-blur'; 
