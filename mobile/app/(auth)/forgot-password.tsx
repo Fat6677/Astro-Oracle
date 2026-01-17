790ea08804eeda0314d692f9e6f446c12d08396f
Fat6677 <diobok345@gmail.com>
2026-01-17 19:57:47 +0700

    feat: menambahkan component untuk forgot password

--- mobile/app/(auth)/forgot-password.tsx
+++ mobile/app/(auth)/forgot-password.tsx
@@ -11,3 +11,10 @@ import {
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 
+
+// Components
+import CosmicBackground from '../../components/Auth/CosmicBackground';
+import AuthCard from '../../components/Auth/AuthCard';
+import CosmicButton from '../../components/Common/CosmicButton';
+
+
