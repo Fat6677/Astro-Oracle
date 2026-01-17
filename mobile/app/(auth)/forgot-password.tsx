16749bae500aa101d6a7227d6a82b531f7a672a1
Fat6677 <diobok345@gmail.com>
2026-01-17 19:57:16 +0700

    feat: menambahkan library yg dibutuhkan forgot password

---
+++ mobile/app/(auth)/forgot-password.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
