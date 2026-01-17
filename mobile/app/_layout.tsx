1ac387d528afa508c45137f9b3159b8a5d6b6458
Fat6677 <diobok345@gmail.com>
2026-01-17 19:35:21 +0700

    update: merubah navigasi ke halaman login

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,12 +1,46 @@
-import { useEffect } from 'react';
-import { Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { PaperProvider } from 'react-native-paper';
 import { CosmicTheme } from '../constants/CosmicTheme';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { CosmicColors } from '../constants/CosmicColors';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  const initializeApp = () => {
+    // Simulasi loading splash screen
+    setTimeout(() => {
+      setIsLoading(false);
+      
+      // langsung ke halaman login
+      router.replace('/(auth)/login');
+    }, 1); // 1.5 detik untuk efek splash screen
+  };
+
+  useEffect(() => {
+    initializeApp();
+  }, []);
+
+  if (isLoading) {
+    // Tampilan splash screen atau loading screen
+    return (
+      <GestureHandlerRootView style={{ flex: 1, backgroundColor: CosmicColors.darker }}>
+        <View style={{ 
+          flex: 1, 
+          justifyContent: 'center', 
+          alignItems: 'center',
+          backgroundColor: CosmicColors.darker 
+        }}>
+          <ActivityIndicator size="large" color={CosmicColors.accent} />
+        </View>
+      </GestureHandlerRootView>
+    );
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <PaperProvider theme={CosmicTheme}>
@@ -15,9 +49,14 @@ export default function RootLayout() {
           backgroundColor={CosmicColors.darker}
         />
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" />
+          <Stack.Screen 
+            name="(auth)" 
+            options={{ 
+              animation: 'fade',
+              gestureEnabled: false 
+            }}
+          />
           <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="index" />
         </Stack>
       </PaperProvider>
     </GestureHandlerRootView>
